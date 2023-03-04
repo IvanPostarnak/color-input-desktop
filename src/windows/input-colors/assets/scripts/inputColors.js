1d037b5c8ea863c1e879cb689c9e18336af84fc5
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-03-04 09:57:43 +0300

    feat: add 'validate input' function

---
+++ src/windows/input-colors/assets/scripts/inputColors.js
@@ -0,0 +1,98 @@
+const { validateInput } = require('validateInput.js');
+
+const MIN_AMOUNT_OF_COLORS = 2;
+let combinationLength = MIN_AMOUNT_OF_COLORS;
+
+let firstInputLine = document.querySelector('.js-1st-input-line');
+let secondInputLine = document.querySelector('.js-2nd-input-line');
+
+    console.log(firstInputLine);
+    console.log(secondInputLine);
+
+let firstLineObject = {
+  lock: firstInputLine.querySelector('.js-lock'),
+  hashtag: firstInputLine.querySelector('.js-hashtag'),
+  input: firstInputLine.querySelector('.js-input'),
+  example: firstInputLine.querySelector('.js-example'),
+  accept: firstInputLine.querySelector('.js-accept'),
+  reject: firstInputLine.querySelector('.js-reject')
+}
+
+let secondLineObject = {
+  lock: secondInputLine.querySelector('.js-lock'),
+  hashtag: secondInputLine.querySelector('.js-hashtag'),
+  input: secondInputLine.querySelector('.js-input'),
+  example: secondInputLine.querySelector('.js-example'),
+  accept: secondInputLine.querySelector('.js-accept'),
+  reject: secondInputLine.querySelector('.js-reject')
+}
+
+    console.log(firstLineObject);
+    console.log(secondLineObject);
+  
+  let validation = false;
+
+firstLineObject.input.addEventListener('input', () => {
+  validation = validateInput(firstLineObject.input.value);
+  console.log(validation);
+});
+
+
+function validateInput(input) {
+  let lowerInputArray = String(input).toLowerCase().split('');
+  let length = lowerInputArray.length;
+
+  let validation = false;
+
+  if (isValidInputLength(length)) {
+    if (length === 7) {
+      let fisrtSymbol = lowerInputArray.shift();
+      if (isValidInputStarter(fisrtSymbol) === false) {
+        return validation;
+      }
+    }
+
+    if (isValidColorCode(lowerInputArray)) {
+      validation = true;
+    }
+  }
+
+  return validation;
+}
+
+function isValidInputLength(length) {
+  const MIN_LENGTH_OF_INPUT = 6
+  const MAX_LENGTH_OF_INPUT = 7
+
+  return length >= MIN_LENGTH_OF_INPUT && length <= MAX_LENGTH_OF_INPUT ? true : false;
+}
+
+function isValidInputStarter(symbol) {
+  const VALID_INPUT_STARTERS = "#";
+
+  return VALID_INPUT_STARTERS.indexOf(symbol) > -1 ? true : false;
+}
+
+function isValidColorCode(arrayOfSymbols) {
+  const VALID_COLOR_SYMBOLS = "0123456789abcdef";
+
+  let length = arrayOfSymbols.length;
+
+  if (isValidColorCodeLength(length)) {
+    return arrayOfSymbols.every((symbol) => {
+      return VALID_COLOR_SYMBOLS.indexOf(symbol) > -1;
+    })
+  } else {
+    return false;
+  }
+}
+
+function isValidColorCodeLength(length) {
+  const LENGTH_OF_COLOR_CODE = 6
+
+  return length === LENGTH_OF_COLOR_CODE ? true : false;
+}
+
+module.export = {
+  validateInput: validateInput
+}
