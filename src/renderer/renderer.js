b3345496f531f10362b52a20b3702b462d342a3b
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-03-16 03:55:06 +0300

    refactor: change 'main-theme-switcher.scss' file

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -4,5 +4,5 @@ const mainThemeSwithcer = document.querySelector('.js-main-theme-switcher');
 mainThemeSwithcer.addEventListener('click', async (event) => {
   event.stopPropagation();
   await window.lightMode.toggle();
-  mainThemeSwithcer.classList.toggle('main-theme-switcher__button--to-light');
+  mainThemeSwithcer.classList.toggle('main-theme-switcher__icon--to-light');
 })
