389cf1a9489de7690aa93a2454b333d4dc0ae4a3
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-03-19 04:05:03 +0300

    fix: change 'is-unique-combination' function to check it properly

--- src/windows/input-colors/assets/scripts/util/combinations/isUniqueCombination.mjs
+++ src/windows/input-colors/assets/scripts/util/combinations/isUniqueCombination.mjs
@@ -1,6 +1,23 @@
+// combination - object of ColorCombination class
+// combinationsHolder - combinationsHolder object itself
 export function isUniqueCombination(combination, combinationsHolder) {
-  let combinationAsString = JSON.stringify(combination);
-  return combinationsHolder.combinations.every((combo) => {
-    return combinationAsString != JSON.stringify(combo);
+  const SINGLE_COLOR_MATCH = 1;
+  const SINGLE_COLOR_NOT_MATCH = 0;
+
+  return combinationsHolder.combinations.every((combinationFromHolder) => {
+    let singleColorMatchingStatuses = [];
+    if (combination.length != combinationFromHolder.length) {
+      return true;
+    } else {
+      combination.colors.forEach((color, index) => {
+        if (combinationFromHolder.colors.includes(color)) {
+          singleColorMatchingStatuses[index] = SINGLE_COLOR_MATCH;
+        } else {
+          singleColorMatchingStatuses[index] = SINGLE_COLOR_NOT_MATCH;
+        }
+      });
+      // false if every color matches
+      return singleColorMatchingStatuses.includes(SINGLE_COLOR_NOT_MATCH);
+    }
   })
 }
